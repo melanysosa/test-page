9ee84dc132799a19dc3a902217cc85c476d913c8
melanysosa <sosamelanynoelia@gmail.com>
2022-10-12 21:04:52 -0300

    footer-modified

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,18 +1,43 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Footer = () => {
+    const navigate = useNavigate();
+	
+	const toTerms = () => navigate('/terms');
+	const toPrivacy = () => navigate('/privacy');
+	const toCookies = () => navigate('/cookies');
 	return (
-		<div className='footer-page h-0  text-white grid grid-cols-3 p-12 tracking-widest '>
+		<div className='footer-page h-0  text-white grid grid-cols-2 p-12 tracking-widest '>
 			<div>
-				<label className='flex w-1 p-2 text-xl '>
-					ASSEMBLE'S TEST PAGE
-				</label>
-				<label className='flex w-0'>
-					Philadelphia, Pensylvania +1 (000) 000 000
-					contact@assemblestudio.com
-				</label>
+				<p className='flex w-1 p-2 text-xl '>ASSEMBLE'S TEST PAGE</p>
+				<p>Philadelphia, Pensylvania</p>
+				<p>+1 (000) 000 000</p>
+				<p>contact@assemblestudio.com</p>
 			</div>
-			<div></div>
+			<div className='flex flex-col justify-end'>
+				<div>
+					<ul className='flex justify-end mb-6'>
+						<li onClick={toTerms} className='mx-2 '>
+							Terms of use |
+						</li>
+
+						<li onClick={toPrivacy} className='mx-2 '>
+							Privacy policy |
+						</li>
+
+						<li onClick={toCookies} className='mx-2 '>
+							Cookies
+						</li>
+					</ul>
+				</div>
+				<div>
+					<p className='flex justify-end text-xs py-2'>
+						Assemble & Partners, Digital Production Studio ©2022
+						Copyright, All Rights Reserved.
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -10,19 +10,19 @@ const NavBar = () => {
 
 	return (
 		<div className='header-nav h-0 w-full text-white grid grid-cols-2 p-12 tracking-widest '>
-			<label onClick={toHome} className=' flex w-1 text-xl'>
+			<p onClick={toHome} className=' flex w-1 text-xl cursor-pointer'>
 				ASSEMBLE'S TEST PAGE
-			</label>
-			<ul className='flex justify-end '>
-				<li onClick={toAbout} className='mx-2 '>
+			</p>
+			<ul className='flex justify-end'>
+				<li onClick={toAbout} className='mx-2 cursor-pointer'>
 					ABOUT |
 				</li>
 
-				<li onClick={toOurWork} className='mx-2 '>
+				<li onClick={toOurWork} className='mx-2 cursor-pointer'>
 					OUR WORK |
 				</li>
 
-				<li onClick={toOurServices} className='mx-2 '>
+				<li onClick={toOurServices} className='mx-2 cursor-pointer'>
 					OUR SERVICES
 				</li>
 			</ul>
